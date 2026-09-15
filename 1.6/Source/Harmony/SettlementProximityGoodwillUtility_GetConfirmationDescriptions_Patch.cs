03a226a9480ede44effdf44d47a7e565a0853461
Taranchuk <nfhfyxer@gmail.com>
2026-09-15 21:42:47 +0300

    patch signature fix

--- 1.6/Source/Harmony/SettlementProximityGoodwillUtility_GetConfirmationDescriptions_Patch.cs
+++ 1.6/Source/Harmony/SettlementProximityGoodwillUtility_GetConfirmationDescriptions_Patch.cs
@@ -8,7 +8,7 @@ namespace VFEInsectoids
     [HarmonyPatch(typeof(SettlementProximityGoodwillUtility), "GetConfirmationDescriptions")]
     public static class SettlementProximityGoodwillUtility_GetConfirmationDescriptions_Patch
     {
-        public static IEnumerable<string> Postfix(IEnumerable<string> result, PlanetTile tile)
+        public static IEnumerable<TaggedString> Postfix(IEnumerable<TaggedString> result, PlanetTile tile)
         {
             foreach (var item in result)
             {
